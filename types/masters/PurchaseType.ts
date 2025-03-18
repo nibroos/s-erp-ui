421c90b4fdc36dc3ef3d30a50b26ca18465958a1
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2025-03-18 16:04:30 +0700

    #1 | Syahira | add type payment term, shipping term, and purchase type

---
+++ types/masters/PurchaseType.ts
@@ -0,0 +1,8 @@
+export type FormPurchaseType = {
+  id?: number | null | undefined | string | string[]
+  name: string
+  code?: string | null
+  description?: string | null
+  remark?: string | null
+  status?: number
+}
