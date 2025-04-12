21b775294e7d985aa5c85e8f5b55cac835b522c4
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2025-04-12 10:13:09 +0700

    #15 | Syahira | integrate ui comp profile

---
+++ stores/masters/CompanyProfileStore.ts
@@ -0,0 +1,266 @@
+import { useAlert } from '~/composables/useAlert'
+import { useMyFetch } from '~/composables/useMyFetch'
+import type { Meta, Pagination, PaginationMeta } from '~/interfaces/LaravelPaginationInterface'
+import type { FormCompanyProfileType } from '~/types/masters/CompanyProfileType'
+
+const useCompanyProfileStore = defineStore('CompanyProfileStore', {
+  state: () => ({
+    form: {
+      id: null,
+      parent_id: null,
+      vat_id: null,
+      pph23_id: null,
+      is_primary: null,
+      payment_id: null,
+      company_name: '',
+      company_owner_name: '',
+      company_sign_name: '',
+      company_city: '',
+      company_province: '',
+      company_district: '',
+      company_postal_code: '',
+      company_address: '',
+      company_phone: '',
+      company_email: '',
+      company_website: '',
+      company_sign: null,
+      company_logo: null,
+      company_description: '',
+      company_remark: '',
+      company_status: 1,
+      company_options_json: null,
+      bank_informations: []
+    } as FormCompanyProfileType,
+    queryModal: {
+      qListIndex: {
+        page: 1,
+        per_page: 10,
+        global: '',
+        order_column: 'company_name',
+        order_direction: 'desc'
+      } as Record<string, any>,
+    },
+    metaModal: {
+      index: {
+        data: [] as any,
+        loading: false,
+        meta: {} as Meta
+      } as PaginationMeta,
+    },
+    tabFormIndex: 0,
+    errors: {} as Record<string, any>,
+    formLoading: false
+  }),
+
+  actions: {
+    async show() {
+      try {
+        if (typeof this.form.id === 'string') {
+          this.form.id = parseInt(this.form.id);
+        }
+        
+        const response = await useMyFetch().post(
+          '/v1/company-profiles/show-company-profile',
+          this.form
+        )
+        this.form = response.data.data[0]
+    
+        return response
+      } catch (error: any) {
+        console.log('Failed To Fetch Data', error.response.data);
+      }
+    },
+
+    async store() {
+      if (!!this.formLoading) return
+      this.formLoading = true
+
+      const isConfirmed = await useAlert.showPopupConfirmation(
+        'Are you sure to save this data?',
+        'Data will be saved'
+      )
+
+      if (!isConfirmed) {
+        this.formLoading = false
+        return
+      }
+
+      try {
+        const formData = new FormData()
+
+        Object.keys(this.form).forEach(key => {
+          if (this.form[key] !== null && this.form[key] !== undefined) {
+            if (key === 'company_sign' && this.form[key] instanceof File) {
+              formData.append(key, this.form[key])
+            } 
+            else if (key === 'company_logo' && this.form[key] instanceof File) {
+              formData.append(key, this.form[key])
+            }
+            else if (key === 'bank_informations' && Array.isArray(this.form[key])) {
+              formData.append(key, JSON.stringify(this.form[key]))
+            }
+            else {
+              formData.append(key, this.form[key])
+            }
+          }
+        })
+
+        const response = await useMyFetch().post(
+          '/v1/company-profiles/create-company-profile',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }
+        )
+        
+        this.form = JSON.parse(
+          JSON.stringify(useInitials.formCompanyProfileCreateEdit)
+        )
+
+        useAlert.hideAlert()
+        useAlert.alertSuccess(response.data.message)
+        navigateTo(`/masters/company-profiles`)
+
+        return response
+      } catch (error: any) {
+        const responseData = error.response.data
+        console.log('Failed To Create Data', error.response.data)
+        let errors = ''
+
+        if (typeof responseData.errors === 'object') {
+          await Promise.all(
+            Object.keys(responseData.errors).map((row: any) => {
+              errors += `- ${responseData.errors[row][0]} <br />`
+              this.errors[row] = responseData.errors[row][0]
+            })
+          )
+        }
+        useAlert.alertError(errors + `<br /> ${responseData.message}`)
+
+        return error.response.data
+      } finally {
+        this.formLoading = false
+      }
+    },
+
+    async update() {
+      if (!!this.formLoading) return
+      this.formLoading = true
+    
+      const isConfirmed = await useAlert.showPopupConfirmation(
+        'Are you sure to save this data?',
+        'Data will be saved'
+      )
+    
+      if (!isConfirmed) {
+        this.formLoading = false
+        return
+      }
+    
+      try {
+        let id = this.form.id
+        const formData = new FormData()
+    
+        Object.keys(this.form).forEach(key => {
+          if (key === 'company_sign' || key === 'company_logo') {
+            if (this.form[key] === null) {
+              formData.append(`${key}_deleted`, '1')
+              formData.append(key, '')
+            } else if (this.form[key] instanceof File) {
+              formData.append(key, this.form[key])
+            } else if (this.form[key]) {
+              formData.append(key, this.form[key])
+            }
+          } 
+          else if (key === 'bank_informations' && Array.isArray(this.form[key])) {
+            formData.append(key, JSON.stringify(this.form[key]))
+          }
+          else if (this.form[key] !== null && this.form[key] !== undefined) {
+            formData.append(key, this.form[key])
+          }
+        })
+    
+        const response = await useMyFetch().post(
+          '/v1/company-profiles/update-company-profile',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          }
+        )
+        
+        this.form = JSON.parse(
+          JSON.stringify(useInitials.formCompanyProfileCreateEdit)
+        )
+    
+        useAlert.hideAlert()
+        useAlert.alertSuccess(response.data.message)
+    
+        navigateTo(`/masters/company-profiles`)
+    
+        return response
+      } catch (error: any) {
+        const responseData = error.response.data
+        console.log('Failed To Update Data', error.response.data)
+        let errors = ''
+    
+        if (typeof responseData.errors === 'object') {
+          await Promise.all(
+            Object.keys(responseData.errors).map((row: any) => {
+              errors += `- ${responseData.errors[row][0]} <br />`
+              this.errors[row] = responseData.errors[row][0]
+            })
+          )
+        }
+        useAlert.alertError(errors + `<br /> ${responseData.message}`)
+    
+        return error.response.data
+      } finally {
+        this.formLoading = false
+      }
+    },
+
+    async delete(id: number | string | string[] | undefined) {
+      this.form.id = id
+      try {
+        const response = await useMyFetch().post(
+          '/v1/company-profiles/delete-company-profile',
+          this.form
+        )
+        this.form = response.data.data[0]
+
+        return response
+      } catch (error: any) {
+        console.log('Failed To Delete Data', error.response.data);
+        useAlert.alertError(error.response.data.message)
+      }
+    },
+
+    async restore(id: number | string | string[] | undefined) {
+      this.form.id = id
+      try {
+        const response = await useMyFetch().post(
+          '/v1/company-profiles/restore-company-profile',
+          this.form
+        )
+        this.form = response.data.data[0]
+
+        return response
+      } catch (error: any) {
+        console.log('Failed To Restore Data', error.response.data);
+        useAlert.alertError(error.response.data.message)
+      }
+    },
+  },
+  persist: [
+    {
+      paths: ['queryModal'],
+      storage: localStorage
+    }
+  ]
+})
+
+export default useCompanyProfileStore
---
+++ types/masters/CompanyProfileType.ts
@@ -0,0 +1,47 @@
+export type FormCompanyProfileType = {
+  id?: number | null | undefined | string | string[]
+  parent_id?: number | null
+  vat_id?: number | null
+  pph23_id?: number | null
+  is_primary?: number | null
+  payment_id?: number | null
+  company_name: string
+  company_owner_name?: string | null
+  company_sign_name?: string | null
+  company_city?: string | null
+  company_province?: string | null
+  company_district?: string | null
+  company_postal_code?: string | null
+  company_address?: string | null
+  company_phone?: string | null
+  company_email?: string | null
+  company_website?: string | null
+  company_sign?: string | File | null
+  company_logo?: string | File | null
+  company_description?: string | null
+  company_remark?: string | null
+  company_status?: number | null
+  company_options_json?: any | null
+  created_by_id?: number | null
+  updated_by_id?: number | null
+  deleted_by_id?: number | null
+  created_at?: string | null
+  updated_at?: string | null
+  deleted_at?: string | null
+  bank_informations?: BankInformationType[]
+}
+
+export type BankInformationType = {
+  id?: number | null | undefined | string | string[]
+  commpany_profile_id?: number | null
+  name?: string | null
+  account_number?: string | null
+  account_name?: string | null
+  description?: string | null
+  created_by_id?: number | null
+  updated_by_id?: number | null
+  deleted_by_id?: number | null
+  created_at?: string | null
+  updated_at?: string | null
+  deleted_at?: string | null
+}
